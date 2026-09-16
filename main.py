aca35e25cb08a73d7991b376fdf3f904a5cc5af8
PavelSBvt <SPS260951@yandex.ru>
2026-09-17 00:23:03 +0300

    > [py] change main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from PyQt6.QtWidgets import QApplication
 from PyQt6.QtGui import QIcon
 
 from core.simple_iteration_method import simple_iteration_method
+from core.utils.function_research import find_bracket
 from gui.apps_window_with_graphics import AppWindow
 from utils.output_rich import Rich
 
@@ -13,6 +14,16 @@ Rich.simple_log("Запуск программы")
 Rich.print_spacer()
 
 if __name__ == "__main__":
+    span = find_bracket(1, 0.0, 100.0, 0.05)
+
+    if span:
+        Rich.simple_log(f"отрезок с единственным "
+                        f"корнем уравнения: [{span[0]:.3f}, {span[1]:.3f}]")
+    else:
+        Rich.warning_log(f"Корень не найден на заданном промежутке")
+
+    Rich.print_spacer()
+
     simple_iteration_method.run()
 
     app = QApplication(sys.argv)
