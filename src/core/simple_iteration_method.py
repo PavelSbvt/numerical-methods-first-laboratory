ed8f0fe3a2f7faa88c3770637b2dd253e9e4385b
PavelSBvt <SPS260951@yandex.ru>
2026-09-17 09:01:40 +0300

    > [py] write comments for code

--- src/core/simple_iteration_method.py
+++ src/core/simple_iteration_method.py
@@ -1,7 +1,5 @@
 from math import sqrt
 
-import matplotlib.pyplot as plt
-
 from utils.output_rich import Rich
 
 
@@ -13,9 +11,12 @@ class SimpleIterationMethod:
     # Метод простой итерации
     # Метод касательных (Ньютона)
     def __init__(self):
+        # необходимая точность приближённого решения
         self.accuracy = 0.001
+        # количество итераций для поиска приближённого решения
         self.max_iter = 10
         self.t = 1
+        # массив со всеми полученными промежуточными (и искомым) приближёнными решениями
         self.list_iterations = []
 
     def run(self, a: float, b: float) -> None:
@@ -39,24 +40,29 @@ class SimpleIterationMethod:
         # проверка сходимости
         self.check_convergence(a, b)
 
-        # x0 = середина отрезка
+        # x0 = середина отрезка - любой x, определяемый как начальное приближение
         x0 = (a + b) / 2
         Rich.debug_log(f"x0 = {x0:.6f}")
 
         # итерации
         self.list_iterations = [x0]
+
         x_prev = x0
+        # Проход по итерациям
         for iteration in range(1, self.max_iter + 1):
             x_next = self.g(x_prev)
             self.list_iterations.append(x_next)
             delta = abs(x_next - x_prev)
+            # промежуточное значение функции для текущего приближённого значения
             residual = abs(self.f(x_next))
 
             Rich.debug_log(
-                f"итерация {iteration:>3}: x = {x_next:.6f}, "
+                f"итерация №{iteration}: x = {x_next:.6f}, "
                 f"|дельта x| = {delta:.6f}, |f(x)| = {residual:.6f}"
             )
 
+            # Проверка на достаточную точность приближённого решения и дельты
+            # (разницы между двумя последними прибл. реш.)
             if delta < self.accuracy and residual < self.accuracy:
                 Rich.print_spacer()
                 Rich.success_log(
@@ -71,15 +77,39 @@ class SimpleIterationMethod:
         return x_prev
 
     def f(self, x: float) -> float:
+        """
+        Функция, вычисляющая значение функции f(x, t) в точке икс
+        :param x: принимает значение переменной икс
+        :return: значение функции f в точке икс
+        """
+
         return 1 / (1 + x ** 4) - x ** 2
 
     def g(self, x: float) -> float:
+        """
+        Функция, вычисляющая значение функции g(x) в точке икс
+        :param x: принимает значение переменной икс
+        :return: значение функции g в точке икс
+        """
         return 1 / sqrt(1 + x ** 4)
 
     def dg(self, x: float) -> float:
+        """
+        Функция, вычисляющая значение производной функции f(x, t) в точке икс
+        :param x: принимает значение переменной икс
+        :return: значение производной функции f в точке икс
+        """
         return -2 * x ** 3 / ((1 + x ** 4) ** 1.5)
 
     def check_convergence(self, a: float, b: float, points: int = 20) -> bool:
+        """
+        Функция для проверки на сходимость метода
+        :param a: левая граница отрезка с единственным корнем
+        :param b: правая граница отрезка с единственным корнем
+        :param points: количество точек, в которых будет находиться и сверяться с 1 значение q
+        :return: bool - True, если метод сходится
+        """
+
         Rich.simple_log("Проверка сходимости метода простой итерации")
         max_q, worst_x = 0.0, a
         for i in range(points + 1):
