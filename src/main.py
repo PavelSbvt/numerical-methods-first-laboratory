f84a036122f2ee4214c73ead00f8a519fb6c83ec
PavelSBvt <SPS260951@yandex.ru>
2026-09-20 23:33:45 +0300

    > [py] change main

--- src/main.py
+++ src/main.py
@@ -15,34 +15,34 @@ Rich.simple_log("Запуск программы")
 Rich.print_spacer()
 
 if __name__ == "__main__":
-    # Поиск и получение отрезка
+    # Поиск и получение отрезка − отделение корня,
+    # т.е. нахождение такого отрезка [a, b], на котором существует
+    # единственный корень уравнения f(x) = 0.
     span = find_bracket(1, 0.0, 2.0, 0.05)
 
     if span:
-        Rich.simple_log(f"отрезок с единственным "
+        Rich.debug_log(f"отрезок с единственным "
                         f"корнем уравнения: [{span[0]:.3f}, {span[1]:.3f}]")
     else:
         Rich.warning_log(f"Корень не найден на заданном промежутке")
 
-    # Границы
+    # Границы отрезка
     a_border = span[0]
     b_border = span[1]
 
     Rich.print_spacer()
 
-    # приближённое решение уравнения
+    # − уточнение корня, т.е. нахождение приближённого решения с заданной
+    # точностью при помощи метода простых итераций
     x_root = simpleIterationMethod.run(a_border, b_border)
 
     app = QApplication(sys.argv)
 
     # окно с графиком и промежуточными решениями тангенциального уравнения
-    # методом простых итераций
+    # методом простых итераций и методом касательных (Ньютона)
     appWindowSimpleIteration = AppWindow(
         a=a_border,
         b=b_border,
-        window_title="Метод простых итераций",
-        chapter="/simple_iteration_method",
-        t=simpleIterationMethod.t,
         x_root=x_root,
         history=simpleIterationMethod.list_iterations,
     )
